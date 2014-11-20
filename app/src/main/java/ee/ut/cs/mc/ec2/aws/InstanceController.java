b9229f56a72529e6aba084ed0273b8cb9da2e8db
Jakob Mass <jaks6@ut.ee>
2014-11-20 23:49:29 +0200

    Renamed InstanceController, added credentials file check

---
+++ app/src/main/java/ee/ut/cs/mc/ec2/aws/InstanceController.java
@@ -0,0 +1,75 @@
+package ee.ut.cs.mc.ec2.aws;
+
+import android.content.Context;
+import android.content.res.Resources;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.PropertiesCredentials;
+import com.amazonaws.services.ec2.AmazonEC2Client;
+import com.amazonaws.services.ec2.model.Instance;
+import com.example.ec2.R;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import ee.ut.cs.mc.ec2.MainActivity;
+
+public class InstanceController {
+
+	private static final String EC2_AWS_CREDENTIALS_PROPERTIES = "/assets/AwsCredentials.properties";
+
+
+
+    String endPoint;
+	AWSCredentials credentials;
+	AmazonEC2Client ec2Client;
+    Instance instance;
+
+    Context context;
+
+	
+	public InstanceController(Context context) throws Exception {
+        context = context.getApplicationContext();
+
+		InputStream credentialsInputStream = getClass().getResourceAsStream(EC2_AWS_CREDENTIALS_PROPERTIES);
+
+		credentials = new PropertiesCredentials(credentialsInputStream);
+        if (credentials.getAWSAccessKeyId().isEmpty() ||
+                credentials.getAWSAccessKeyId().isEmpty()){
+            throw new Exception(context.getString(R.string.aws_credentials_missing_error));
+        } else {
+            ec2Client = new AmazonEC2Client(credentials);
+
+            //By default, the service endpoint is ec2.us-east-1.amazonaws.com.
+            if(!endPoint.isEmpty()) ec2Client.setEndpoint(endPoint);
+        }
+	}
+
+
+    public void terminateInstance() {
+        if (instance != null){
+            TerminateInstanceTask instanceTask = new TerminateInstanceTask();
+            instanceTask.execute(this);
+        }
+    }
+
+    public void launchInstance(LaunchConfiguration conf, MainActivity activity) {
+        InstanceLauncher launcher = new InstanceLauncher(this,conf);
+        if (instance == null){
+            RunInstanceTask instanceTask = new RunInstanceTask(activity);
+            instanceTask.execute(launcher);
+        }
+    }
+
+
+    public AmazonEC2Client getEc2Client() {
+        return ec2Client;
+    }
+    public Instance getInstance() {
+        return instance;
+    }
+    public void setInstance(Instance instance) {
+        this.instance = instance;
+    }
+    public void setEndPoint(String endPoint) {this.endPoint = endPoint; }
+}
